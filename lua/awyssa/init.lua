e4f944b4fcb5d74fc73a2aab8467c56519778597
awyssa <michael.ehson@gmail.com>
2025-03-02 21:10:40 +0000

    init

--- lua/awyssa/init.lua
+++ lua/awyssa/init.lua
@@ -1,3 +1,9 @@
 require("awyssa.remap")
 require("awyssa.lazy")
 require("awyssa.set")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        require("nvim-tree.api").tree.open()
+    end
+})
--- lua/awyssa/remap.lua
+++ lua/awyssa/remap.lua
@@ -1,8 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
 -- Improve movement commands
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -14,6 +12,37 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "=ap", "ma=ap'a")
+
+-- Keep current copied word
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Copy to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
+
+-- No...
+vim.keymap.set("n", "Q", "<nop>")
+
+-- Move back a terminal session
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- LSP formatter
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- Quickfix list
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Find and replace the word I was on
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>")
 
 -- Snippits
 vim.keymap.set("i", "<leader>clog", "console.log()")
